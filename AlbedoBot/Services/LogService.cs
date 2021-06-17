cab54f14b02b3c6e54ba7f226de2a8a22b5cfc6e
Max <rendon4ik@gmail.com>
2021-06-17 14:53:25 +0300

    Temporary LogService class initialization

---
+++ AlbedoBot/Services/LogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+
+namespace AlbedoBot.Services
+{
+    public static class LogService
+    {
+        public static async Task LogAsync(LogMessage logMessage)
+        {
+            Console.WriteLine($"[LOG]   {logMessage}");
+        }
+
+        public static async Task InfoAsync(string info)
+        {
+            Console.WriteLine($"[INFO]   {info}");
+        }
+
+        public static async Task ExceptionAsync(Exception exception)
+        {
+            Console.WriteLine($"[EXCEPTION]   {exception.Message}");
+        }
+    }
+}
