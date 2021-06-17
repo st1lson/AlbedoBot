bb6f9cb5f35fcdf9cf68dc10cf3795cfc5b940e9
Max <rendon4ik@gmail.com>
2021-06-17 14:54:03 +0300

    Adding Config class

---
+++ AlbedoBot/Core/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace AlbedoBot.Core
+{
+    public class Config
+    {
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+        public string GameStatus { get; set; }
+    }
+}
