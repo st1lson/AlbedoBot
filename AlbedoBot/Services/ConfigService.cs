df47f24adc91b99aafeb1a3800405f96d9051849
Max <rendon4ik@gmail.com>
2021-06-17 14:52:23 +0300

    Initialize ConfigService class

--- AlbedoBot/Services/ConfigService.cs
+++ AlbedoBot/Services/ConfigService.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Threading.Tasks;
+using AlbedoBot.Core;
+using Newtonsoft.Json;
 
 namespace AlbedoBot.Services
 {
-    class ConfigService
+    public class ConfigService
     {
+        public static string Path { get; set; }
+        public static Config Config { get; set; }
+
+        public ConfigService()
+        {
+            Path = "config.json";
+        }
+
+        public async Task InitializeAsync()
+        {
+            if (!File.Exists(Path))
+            {
+                await CreateFile();
+            }
+            var data = File.ReadAllText(Path);
+            Config = JsonConvert.DeserializeObject<Config>(data);
+        }
+
+        private Task CreateFile()
+        {
+            var config = new Config
+            {
+                Token = "",
+                Prefix = "!",
+                GameStatus = ""
+            };
+
+            var data = JsonConvert.SerializeObject(config);
+            File.WriteAllText(Path, data);
+            return Task.CompletedTask;
+        }
     }
 }
