02ed891be9c9afa55af28874c960168951abd881
Max <rendon4ik@gmail.com>
2021-06-21 21:44:09 +0300

    Adding casual text commands

---
+++ AlbedoBot/Modules/TextModule.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace AlbedoBot.Modules
+{
+    public sealed class TextModules : ModuleBase<SocketCommandContext>
+    {
+        [Command("hi")]
+
+        public async Task Hello()
+        {
+            var user = Context.User;
+
+            await ReplyAsync($"**Welcome back,** {user.Mention}");
+        }
+
+        [Command("roll")]
+
+        public async Task Roll([Remainder] int upper = 100)
+        {
+            Random random = new Random();
+
+            await ReplyAsync($"**You got** `{random.Next(upper)}/{upper}`");
+        }
+
+        [Command("flip")]
+
+        public async Task Flip()
+        {
+            Random random = new Random();
+
+            int flip = random.Next(2);
+
+            if (flip == 0)
+            {
+                await ReplyAsync("**Heads**");
+            }
+            else
+            {
+                await ReplyAsync("**Tails**");
+            }
+        }
+    }
+}
