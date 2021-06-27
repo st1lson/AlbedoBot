636f4cc68eaa9379f866cf76245b8cfd01f6124e
Max <rendon4ik@gmail.com>
2021-06-27 12:23:15 +0300

    Added alias for music module commands

--- AlbedoBot/Modules/MusicModule.cs
+++ AlbedoBot/Modules/MusicModule.cs
@@ -11,6 +11,7 @@ namespace AlbedoBot.Modules
         public MusicService MusicService { get; set; }
 
         [Command("join")]
+        [Summary("Command to join bot to your voice channel")]
 
         public async Task Join()
         {
@@ -22,21 +23,20 @@ namespace AlbedoBot.Modules
         }
 
         [Command("play")]
+        [Alias("p", "track")]
+        [Summary("Command to play a track")]
 
         public async Task Play([Remainder] string trackTitle)
         {
             var user = Context.User as SocketGuildUser;
 
-            if (user is null) return;
-            
-            if (!MusicService.Joined(user.Guild))
+            if (user is not null && !MusicService.Joined(user.Guild))
             {
-                string joinResult = await MusicService.JoinAsync(Context.Guild, user.VoiceChannel, Context.Channel as ITextChannel);
+                var joinResult = await MusicService.JoinAsync(Context.Guild, user.VoiceChannel, Context.Channel as ITextChannel);
 
                 await ReplyAsync(joinResult);
 
-                if (joinResult.Equals(":no_entry_sign: **You need to join to a voice channel!**")
-                    || joinResult.Equals(":no_entry_sign: **I'm already connected to a voice channel!**"))
+                if (joinResult.Equals(":no_entry_sign: **You need to join to a voice channel!**") || joinResult.Equals(":no_entry_sign: **I'm already connected to a voice channel!**"))
                 {
                     return;
                 }
@@ -47,7 +47,27 @@ namespace AlbedoBot.Modules
             await ReplyAsync(embed: await MusicService.PlayAsync(user, Context.Guild, trackTitle));
         }
 
+        [Command("pause")]
+        [Alias("stop")]
+        [Summary("Command to pause current track")]
+
+        public async Task Pause()
+        {
+            await ReplyAsync(await MusicService.PauseAsync(Context.Guild));
+        }
+
+        [Command("resume")]
+        [Alias("continue")]
+        [Summary("Command to resume paused track")]
+
+        public async Task Resume()
+        {
+            await ReplyAsync(await MusicService.ResumeAsync(Context.Guild));
+        }
+        
         [Command("skip")]
+        [Alias("s", "next")]
+        [Summary("Command to skip current track")]
 
         public async Task Skip()
         {
@@ -55,27 +75,34 @@ namespace AlbedoBot.Modules
         }
 
         [Command("leave")]
+        [Summary("Command to force the bot to leave current voice channel")]
 
         public async Task Leave()
         {
             await ReplyAsync(await MusicService.LeaveAsync(Context.Guild));
         }
 
+        [Command("queue")]
+        [Alias("tracks", "list")]
+        [Summary("Command to check the queue")]
+
+        public async Task Queue()
+        {
+            await ReplyAsync(embed: await MusicService.QueueAsync(Context.Guild));
+        }
+        
         [Command("left")]
+        [Alias("time")]
+        [Summary("Command to check the time to the end of the current track")]
 
         public async Task Left()
         {
             await ReplyAsync(await MusicService.LeftAsync(Context.Guild));
         }
 
-        [Command("queue")]
-
-        public async Task Queue()
-        {
-            await ReplyAsync(embed: await MusicService.QueueAsync(Context.Guild));
-        }
-
         [Command("now")]
+        [Alias("current", "playing")]
+        [Summary("Command to check the current track information")]
 
         public async Task Now()
         {
@@ -83,13 +110,17 @@ namespace AlbedoBot.Modules
         }
 
         [Command("repeat")]
+        [Alias("loop")]
+        [Summary("Command to repeat the current track")]
 
         public async Task Repeat()
         {
             await ReplyAsync(await MusicService.RepeatAsync(Context.Guild));
         }
 
+
         [Command("clear")]
+        [Summary("Command to clear the queue")]
 
         public async Task Clear()
         {
