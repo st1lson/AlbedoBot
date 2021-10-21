be4b118806e0d0bde0ce5aaacf454ffbc9a6f09f
Max <rendon4ik@gmail.com>
2021-10-21 14:00:17 +0300

    Change TrackEnded event handler

--- AlbedoBot/Services/MusicService.cs
+++ AlbedoBot/Services/MusicService.cs
@@ -502,26 +502,32 @@ namespace AlbedoBot.Services
 
         public async Task TrackEnded(TrackEndedEventArgs trackEnded)
         {
-            if (!trackEnded.Reason.ShouldPlayNext())
+            if (trackEnded.Reason != TrackEndReason.Finished && trackEnded.Reason != TrackEndReason.Stopped)
             {
                 return;
             }
 
-            if (_repeatTokens.TryGetValue(trackEnded.Player.VoiceChannel.Id, out var repeat) && repeat)
+            var player = trackEnded.Player;
+            if (_repeatTokens.TryGetValue(player.VoiceChannel.Id, out var repeat) && repeat)
             {
                 var currentTrack = trackEnded.Track;
-                await trackEnded.Player.PlayAsync(currentTrack);
+                await player.PlayAsync(currentTrack);
                 return;
             }
             
-            if (!trackEnded.Player.Queue.TryDequeue(out var track))
+            if (!player.Queue.TryDequeue(out var track))
             {
-                await InitiateDisconnectAsync(trackEnded.Player, TimeSpan.FromMinutes(5));
+                _ = InitiateDisconnectAsync(player, TimeSpan.FromMinutes(5));
                 return;
             }
 
-            _timeLeft[trackEnded.Player.VoiceChannel.Id] -= trackEnded.Track.Duration;
-            await trackEnded.Player.PlayAsync(track);
+            if (track is null)
+            {
+                return;
+            }
+
+            _timeLeft[player.VoiceChannel.Id] -= trackEnded.Track.Duration;
+            await player.PlayAsync(track);
         }
 
         public async Task TrackStarted(TrackStartEventArgs trackStarted)
